38ef00474cba3ac607e506c6b6c7d2bf760fa1d8
Oswaldo Montaño <oswaldoeliezer876@gmail.com>
2021-01-14 22:49:56 -0400

    myapp/view.py: Se crea visrta index.html
    Se crea la vista  con sus respectivas consultas al modelo Post

--- project/myapp/myapp/views.py
+++ project/myapp/myapp/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.apps import apps
+
 
 def home(request):
-    return render(request, 'index.html')
+    #post = Post.objects.filter(outstandingType1=True)
+    ModelPost = apps.get_model('blog', 'Post')
+    verticalCard = ModelPost.objects.filter(category=8)[:4]
+    outstandingType1 = ModelPost.objects.filter(outstandingType1=True)
+    outstandingType2 = ModelPost.objects.filter(outstandingType2=True)
+    context = {
+        'verticalCard' : verticalCard,
+        'post': outstandingType1,
+        'postHot': outstandingType2
+    }
+    return render(request, 'index.html', context)
