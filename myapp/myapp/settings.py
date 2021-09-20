99641efbc6291a19dbe9844d309e1b29a6e7bf63
Oswaldo Montaño <oswaldoeliezer876@gmail.com>
2021-09-20 16:42:00 -0400

    Statics files

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -145,7 +145,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Static files (CSS, JavaScript, Images)
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
