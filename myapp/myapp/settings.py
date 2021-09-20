e3f0e88ca649f36399dbaba90142867858d6b736
Oswaldo Montaño <oswaldoeliezer876@gmail.com>
2021-09-20 16:31:57 -0400

    Statics files

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -4,7 +4,7 @@ import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -146,6 +146,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Static files (CSS, JavaScript, Images)
 
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
